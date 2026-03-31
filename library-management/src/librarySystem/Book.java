36edd8646154130e819eda50bae67c99a9e08357
Saugat <sau.gat8602@gmail.com>
2026-03-31 15:38:13 +0545

    Added Book class

--- library-management/src/librarySystem/Book.java
+++ library-management/src/librarySystem/Book.java
@@ -1,4 +1,35 @@
 package librarySystem;
 
 public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public void addBook(String id, String title, String author){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public boolean isItAvailable(){
+        return this.isAvailable;
+    }
+
+    public void letBeBorrowed(){
+        this.isAvailable = false;
+    }
 }
